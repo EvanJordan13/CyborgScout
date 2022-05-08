5b8197d664ca9b7ba9c53dc2bd4fb7effe34939f
EvanJ <ejordan1305@gmail.com>
2022-05-08 16:03:52 -0500

    Initial Commit

---
+++ FRC Scouting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FRC Scouting
+//
+//  Created by Evan Jordan on 5/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FRC Scouting/FRC_ScoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRC_ScoutingApp.swift
+//  FRC Scouting
+//
+//  Created by Evan Jordan on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FRC_ScoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
